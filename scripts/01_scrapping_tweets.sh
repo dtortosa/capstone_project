1385926be99e20039803849722b82416e779ebc9
dtortosa <dftortosa@gmail.com>
2022-07-29 18:51:29 -0700

    adding previous sentiment

--- scripts/01_scrapping_tweets.sh
+++ scripts/01_scrapping_tweets.sh
@@ -50,6 +50,20 @@ cd /media/dftortosa/Windows/Users/dftor/Documents/diego_docs/industry/data_incub
 #get tweets searching "european central bank"
 cd data/json_files
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "european central bank until:2022-07-17" > search_euro_bank_twitter_1999_1_1.json
+	#[GLOBAL-OPTIONS]
+			#--jsonl: get the output as JSON
+			#--progress: show progress on stderr
+			#--max-results: return only the first N results
+			#--since DATETIME: Only return newer than DATETIME
+		#SCRAPER NAME
+			#twitter-search
+			#twitter-hashtag
+			#reddit-search
+			#etc...
+		#[SCRAPER-OPTIONS]
+		#Note that
+			#Note that --since is an optional argument you can use, but until: is an operator used inside the twitter-search query. This seems to give me more consistent results than putting both arguments inside the query.
+				#https://betterprogramming.pub/how-to-scrape-tweets-with-snscrape-90124ed006af
 snscrape --jsonl --progress --since 1999-1-1 twitter-hashtag "ecb until:2022-07-17" > hashtag_euro_bank_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "EU debt crisis until:2022-07-17" > search_eu_debt_crisis_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "european union economy until:2022-07-17" > search_european_union_economy_twitter_1999_1_1.json
@@ -61,27 +75,13 @@ snscrape --jsonl --progress --since 1999-1-1 twitter-search "Greece economy unti
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "Spain economy until:2022-07-17" > search_spain_economy_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "Cyprus economy until:2022-07-17" > search_cyprus_economy_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "Slovenia economy until:2022-07-17" > search_slovenia_economy_twitter_1999_1_1.json
-snscrape --jsonl --progress --since 1999-1-1 twitter-search "Germany economy until:2022-07-17" > search_germany_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "France economy until:2022-07-17" > search_france_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Belgium economy until:2022-07-17" > search_belgium_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Croatia economy until:2022-07-17" > search_croatia_economy_twitter_1999_1_1.json
+
 
 snscrape --jsonl --progress --since 2003-11-1 twitter-search "Jean-Claude Trichet until:2011-10-31" > search_trichet_twitter.json
 snscrape --jsonl --progress --since 2011-06-24 twitter-search "Mario Draghi until:2019-10-31" > search_draghi_twitter.json
 	#starting when Draghi was finally selected as president.
 snscrape --jsonl --progress --since 2019-09-17 twitter-search "Christine Lagarde until:2022-07-17" > search_lagarde_twitter.json
 	#starting when Lagarde was finally selected as president.
-
-
-cd ../..
-	#[GLOBAL-OPTIONS]
-		#--jsonl: get the output as JSON
-		#--progress: show progress on stderr
-		#--max-results: return only the first N results
-		#--since DATETIME: Only return newer than DATETIME
-	#SCRAPER NAME
-		#twitter-search
-		#twitter-hashtag
-		#reddit-search
-		#etc...
-	#[SCRAPER-OPTIONS]
-	#Note that
-		#Note that --since is an optional argument you can use, but until: is an operator used inside the twitter-search query. This seems to give me more consistent results than putting both arguments inside the query.
-			#https://betterprogramming.pub/how-to-scrape-tweets-with-snscrape-90124ed006af
