0ef8797a828b08e485020365b3b66840fd240f5a
dtortosa <dftortosa@gmail.com>
2022-08-30 12:43:33 -0700

    adding sentiment around USD

--- scripts/01_scrapping_tweets.sh
+++ scripts/01_scrapping_tweets.sh
@@ -65,8 +65,8 @@ snscrape --jsonl --progress --since 1999-1-1 twitter-search "european central ba
 			#Note that --since is an optional argument you can use, but until: is an operator used inside the twitter-search query. This seems to give me more consistent results than putting both arguments inside the query.
 				#https://betterprogramming.pub/how-to-scrape-tweets-with-snscrape-90124ed006af
 snscrape --jsonl --progress --since 1999-1-1 twitter-hashtag "ecb until:2022-08-25" > hashtag_euro_bank_twitter_1999_1_1.json
-snscrape --jsonl --progress --since 1999-1-1 twitter-search "EU debt crisis until:2022-08-25" > search_eu_debt_crisis_twitter_1999_1_1.json
 
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "EU debt crisis until:2022-08-25" > search_eu_debt_crisis_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "european union economy until:2022-08-25" > search_european_union_economy_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "Iceland economy until:2022-08-25" > search_iceland_economy_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "Portugal economy until:2022-08-25" > search_portugal_economy_twitter_1999_1_1.json
@@ -79,6 +79,17 @@ snscrape --jsonl --progress --since 1999-1-1 twitter-search "Slovenia economy un
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "France economy until:2022-08-25" > search_france_economy_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "Belgium economy until:2022-08-25" > search_belgium_economy_twitter_1999_1_1.json
 snscrape --jsonl --progress --since 1999-1-1 twitter-search "Croatia economy until:2022-08-25" > search_croatia_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Germany economy until:2022-08-25" > search_germany_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Netherlands economy until:2022-08-25" > search_netherlands_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Finland economy until:2022-08-25" > search_finland_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Austria economy until:2022-08-25" > search_austria_economy_twitter_1999_1_1.json
+
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Luxembourg economy until:2022-08-25" > search_luxembourg_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Slovakia economy until:2022-08-25" > search_slovakia_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Malta economy until:2022-08-25" > search_malta_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Estonia economy until:2022-08-25" > search_estonia_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Latvia economy until:2022-08-25" > search_latvia_economy_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Lithuania economy until:2022-08-25" > search_lithuania_economy_twitter_1999_1_1.json
 
 
 snscrape --jsonl --progress --since 2003-11-1 twitter-search "Jean-Claude Trichet until:2011-10-31" > search_trichet_twitter.json
@@ -87,3 +98,15 @@ snscrape --jsonl --progress --since 2011-06-24 twitter-hashtag "Draghi until:201
 snscrape --jsonl --progress --since 2019-09-17 twitter-hashtag "Lagarde until:2022-08-25" > hashtag_lagarde_twitter.json
 	#starting when Lagarde was finally selected as president.
 #The searches for tweets about Draghi and Lagarde do not work, so I looked for hashtags instead.
+
+
+
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "Federal Reserve until:2022-08-25" > search_fed_twitter_1999_1_1.json
+	#stopped early
+snscrape --jsonl --progress --since 1999-1-1 twitter-hashtag "fed until:2022-08-25" > hashtag_fed_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 1999-1-1 twitter-search "US economy until:2022-08-25" > search_us_economy_twitter_1999_1_1.json
+	#stopped early
+snscrape --jsonl --progress --since 2006-2-1 twitter-search "Ben S. Bernanke until:2014-01-31" > search_bernanke_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 2014-2-1 twitter-search "Janet L. Yellen until:2018-01-31" > search_yellen_twitter_1999_1_1.json
+snscrape --jsonl --progress --since 2018-2-1 twitter-search "Jerome H. Powell until:2022-08-25" > search_powell_twitter_1999_1_1.json
+
